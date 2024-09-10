6160ec9b142ff9e3c27c34d322f6bc3eb51f8b91
alexsvorada <alex@svorada.eu>
2024-09-10 22:34:41 +0200

    feat: added @controlleradvice exceptionhandler

--- src/main/java/eu/svorada/alex/todo/utilities/GlobalExceptionHandler.java
+++ src/main/java/eu/svorada/alex/todo/utilities/GlobalExceptionHandler.java
@@ -1,3 +1,23 @@
 package eu.svorada.alex.todo.utilities;
 
-public class GlobalExceptionHandler {}
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+  @ExceptionHandler(EntityNotFoundException.class)
+  @ResponseStatus(HttpStatus.NOT_FOUND)
+  @ResponseBody
+  public ProblemDetail handleEntityNotFoundException(final EntityNotFoundException e) {
+    ProblemDetail problemDetail = ProblemDetail.forStatus(HttpStatus.NOT_FOUND);
+    problemDetail.setTitle("Entity Not Found");
+    problemDetail.setDetail(e.getMessage());
+    return problemDetail;
+  }
+}
