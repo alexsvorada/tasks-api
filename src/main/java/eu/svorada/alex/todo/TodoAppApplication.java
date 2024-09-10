3721ea351db14a225008dc476d7a5802a834661b
alexsvorada <alex@svorada.eu>
2024-09-10 21:40:57 +0200

    chore(todo): add logging

--- src/main/java/eu/svorada/alex/todo/TodoAppApplication.java
+++ src/main/java/eu/svorada/alex/todo/TodoAppApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TodoAppApplication {
 
+	//TODO Add logging code-wide
 	public static void main(String[] args) {
 		SpringApplication.run(TodoAppApplication.class, args);
 	}
