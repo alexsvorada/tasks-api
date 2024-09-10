55e6b70bf4880e2b03d8a99a3257770e818d0847
alexsvorada <alex@svorada.eu>
2024-09-10 22:40:44 +0200

    chore(todo): implement h2 database and tests

--- src/main/java/eu/svorada/alex/todo/TodoAppApplication.java
+++ src/main/java/eu/svorada/alex/todo/TodoAppApplication.java
@@ -8,6 +8,7 @@ public class TodoAppApplication {
 
 	//TODO Add logging code-wide
 	//TODO Add validation for Task entity
+	//TODO Add h2 database and tests
 	public static void main(String[] args) {
 		SpringApplication.run(TodoAppApplication.class, args);
 	}
