ab01204b3abdcefbbb83c9eb4b78b9daf97bdc90
alexsvorada <alex@svorada.eu>
2024-09-12 11:00:23 +0200

    bugfix: change to interface

--- src/main/java/eu/svorada/alex/todo/repositories/TaskRepository.java
+++ src/main/java/eu/svorada/alex/todo/repositories/TaskRepository.java
@@ -1,6 +1,8 @@
 package eu.svorada.alex.todo.repositories;
 
 import eu.svorada.alex.todo.models.Task;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Component;
 
-public abstract class TaskRepository implements JpaRepository<Task, Long> {}
+public interface TaskRepository extends JpaRepository<Task, Long> {}
